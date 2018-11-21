5f58bf8cdd2bba333921e6995e3b61e08d9582eb
Victor Derks <vbaderks@gmail.com>
2018-11-21 23:03:10 +0100

    Move the C# projects from the net to dotnet/src and dotnet/test folders

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Team CharLS. All rights reserved. See the accompanying "LICENSE.md" for licensed use.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
---
+++ src/JpegLSPresetCodingParameters.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Team CharLS. All rights reserved. See the accompanying "LICENSE.md" for licensed use.
+
+using System.Runtime.InteropServices;
+
+namespace CharLS
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    internal struct JpegLSPresetCodingParameters
+    {
+        internal int MaximumSampleValue;
+        internal int Threshold1;
+        internal int Threshold2;
+        internal int Threshold3;
+        internal int ResetValue;
+    }
+}
