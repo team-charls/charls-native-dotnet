f5034f4791cd7be52b29a81c61441996e6a0d56a
Victor Derks <vbaderks@gmail.com>
2020-11-12 00:05:59 +0100

    Update sample to c# 9.0 top level program
    With .NET 5.0 out of preview the sample can be converted to a top level app.

--- samples/Convert/Program.cs
+++ samples/Convert/Program.cs
@@ -8,103 +8,98 @@ using System.Drawing.Imaging;
 using System.IO;
 using CharLS.Native;
 
-internal class Program
+const int Success = 0;
+const int Failure = 1;
+
+// This sample demonstrates how to convert another encoded image to a JPEG-LS encoded image.
+// The input path should be an absolute path to a file format .NET can read (.bmp, .png, etc).
+if (!TryParseArguments(args, out string inputPath))
 {
-    private const int Success = 0;
-    private const int Failure = 1;
+    Console.WriteLine("Usage: Convert <path to image file>");
+    return Failure;
+}
 
-    private static int Main(string[] args)
+try
+{
+    using Bitmap image = new(inputPath);
+
+    var bitmapData = image.LockBits(
+        new(0, 0, image.Width, image.Height),
+        ImageLockMode.ReadWrite,
+        PixelFormat.Format24bppRgb);
+
+    if (bitmapData.Stride < 0)
     {
-        // This sample demonstrates how to convert another encoded image to a JPEG-LS encoded image.
-        // The input path should be an absolute path to a file format .NET can read (.bmp, .png, etc).
-        if (!TryParseArguments(args, out string inputPath))
-        {
-            Console.WriteLine("Usage: Convert <path to image file>");
-            return Failure;
-        }
-
-        try
-        {
-            using Bitmap image = new(inputPath);
-
-            var bitmapData = image.LockBits(
-                new(0, 0, image.Width, image.Height),
-                ImageLockMode.ReadWrite,
-                PixelFormat.Format24bppRgb);
-            if (bitmapData.Stride < 0)
-            {
-                Console.WriteLine("Image {inputPath} is not top down.");
-                return Failure;
-            }
-
-            Span<byte> pixels;
-            unsafe
-            {
-                pixels = new(bitmapData.Scan0.ToPointer(), bitmapData.Stride * image.Height);
-            }
-
-            // GDI+ returns bgr pixels, JPEG-LS (Spiff) only knows RGB as color space.
-            ConvertBgrToRgb(pixels, image.Width, image.Height, bitmapData.Stride);
-
-            using JpegLSEncoder jpeglsEncoder = new(bitmapData.Width, bitmapData.Height, 8, 3)
-            {
-                InterleaveMode = JpegLSInterleaveMode.Sample
-            };
-
-            jpeglsEncoder.WriteStandardSpiffHeader(SpiffColorSpace.Rgb);
-            jpeglsEncoder.Encode(pixels, bitmapData.Stride);
-
-            Save(GetOutputPath(inputPath), jpeglsEncoder.EncodedData.Span);
-
-            return Success;
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine("Error: " + e.Message);
-        }
-
+        Console.WriteLine("Image {inputPath} is not top down.");
         return Failure;
     }
 
-    private static string GetOutputPath(string inputPath)
+    Span<byte> pixels;
+    unsafe
     {
-        return Path.ChangeExtension(inputPath, ".jls");
+        pixels = new(bitmapData.Scan0.ToPointer(), bitmapData.Stride * image.Height);
     }
 
-    private static void Save(string path, ReadOnlySpan<byte> encodedData)
+    // GDI+ returns bgr pixels, JPEG-LS (Spiff) only knows RGB as color space.
+    ConvertBgrToRgb(pixels, image.Width, image.Height, bitmapData.Stride);
+
+    using JpegLSEncoder jpeglsEncoder = new(bitmapData.Width, bitmapData.Height, 8, 3)
     {
-        using FileStream output = new(path, FileMode.OpenOrCreate);
-        output.Write(encodedData);
-    }
+        InterleaveMode = JpegLSInterleaveMode.Sample
+    };
 
-    private static bool TryParseArguments(IReadOnlyList<string> args, out string inputPath)
+    jpeglsEncoder.WriteStandardSpiffHeader(SpiffColorSpace.Rgb);
+    jpeglsEncoder.Encode(pixels, bitmapData.Stride);
+
+    Save(GetOutputPath(inputPath), jpeglsEncoder.EncodedData.Span);
+
+    return Success;
+}
+catch (IOException e)
+{
+    Console.WriteLine("Error: " + e.Message);
+}
+
+return Failure;
+
+string GetOutputPath(string inputPath)
+{
+    return Path.ChangeExtension(inputPath, ".jls");
+}
+
+void Save(string path, ReadOnlySpan<byte> encodedData)
+{
+    using FileStream output = new(path, FileMode.OpenOrCreate);
+    output.Write(encodedData);
+}
+
+bool TryParseArguments(IReadOnlyList<string> args, out string inputPath)
+{
+    inputPath = string.Empty;
+
+    if (args.Count != 1)
+        return false;
+
+    inputPath = args[0];
+    return true;
+}
+
+void ConvertBgrToRgb(Span<byte> pixels, int width, int height, int stride)
+{
+    const int bytesPerRgbPixel = 3;
+
+    for (int line = 0; line < height; ++line)
     {
-        inputPath = string.Empty;
-
-        if (args.Count != 1)
-            return false;
-
-        inputPath = args[0];
-        return true;
-    }
-
-    private static void ConvertBgrToRgb(Span<byte> pixels, int width, int height, int stride)
-    {
-        const int bytesPerRgbPixel = 3;
-
-        for (int line = 0; line < height; ++line)
+        int lineStart = line * stride;
+        for (int pixel = 0; pixel < width; ++pixel)
         {
-            int lineStart = line * stride;
-            for (int pixel = 0; pixel < width; ++pixel)
-            {
-                int column = pixel * bytesPerRgbPixel;
-                int a = lineStart + column;
-                int b = lineStart + column + 2;
+            int column = pixel * bytesPerRgbPixel;
+            int a = lineStart + column;
+            int b = lineStart + column + 2;
 
-                byte temp = pixels[a];
-                pixels[a] = pixels[b];
-                pixels[b] = temp;
-            }
+            byte temp = pixels[a];
+            pixels[a] = pixels[b];
+            pixels[b] = temp;
         }
     }
 }
