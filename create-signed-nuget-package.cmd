917ae0d29e68f9033c6a07e863ef421bed6b1b43
Victor Derks <vbaderks@gmail.com>
2024-11-12 23:05:13 +0100

    Execute nuget restore before clean (#81)
    Executing clean could fail when building with .NET 9.0 SDK.

--- create-signed-nuget-package.cmd
+++ create-signed-nuget-package.cmd
@@ -1,3 +1,3 @@
-msbuild -t:clean -p:Configuration=Release
 msbuild -t:restore -p:Configuration=Release
+msbuild -t:clean -p:Configuration=Release
 msbuild -t:pack -p:Configuration=Release -p:ContinuousIntegrationBuild=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
